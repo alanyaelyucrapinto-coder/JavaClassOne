dfb81992c4e69ece3569a94badffdfd1698752b3
alanyaelyucrapinto <alanyaelyucrapinto@gmail.com>
2026-09-07 01:48:53 -0500

    Algoritmo Native

---
+++ src/main/java/pe/edu/upeu/algoritmosgui/Launcher.java
@@ -0,0 +1,10 @@
+package pe.edu.upeu.algoritmosgui;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+
+        Application.launch(MatrizTriangular.class, args);
+    }
+}
